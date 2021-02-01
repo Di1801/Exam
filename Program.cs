eaa72b2f8fecef0678a5c09fae67724b65f6375f
Di1801 <78206722+Di1801@users.noreply.github.com>
2021-02-01 16:01:19 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_PPM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello World");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
